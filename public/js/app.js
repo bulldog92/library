1ffa3c4bc91017af7bfe479107fb0009c9f2294e
skost <skost@syneforge.com>
2016-01-27 16:59:53 +0000

    initial first

---
+++ public/js/app.js
@@ -0,0 +1,70 @@
+
+var app = angular.module('libraryApp', [
+	'ui.router',
+	'satellizer',
+	'ngAnimate',
+	'ngMaterial'
+]);
+
+app.config(function($stateProvider, $urlRouterProvider, $authProvider){
+	// satellizer config
+	// $authProvider.facebook({
+	// 	clientId: 'Facebook App ID'
+	// });
+
+    // define routes
+	$stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "templates/main.html",
+			controller: 'MainCtrl'
+		})
+		.state('register', {
+			url: "/register",
+			templateUrl: "templates/register.html",
+			controller: 'AuthCtrl',
+			resolve: {
+	          skipIfLoggedIn: skipIfLoggedIn
+	        }
+		})
+		.state('login', {
+			url: "/login",
+			templateUrl: "templates/login.html",
+			controller: 'AuthCtrl'
+		})
+		.state('logout', {
+			url: '/logout',
+			template: null,
+			controller: 'LogoutCtrl'
+		})
+		.state('profile', {
+			url: '/profile',
+			templateUrl: 'templates/profile.html',
+			controller: 'ProfileCtrl',
+			resolve: {
+				loginRequired: loginRequired
+			}
+		});
+
+	$urlRouterProvider.otherwise("/");
+
+	// Resolve functions
+	function skipIfLoggedIn($q, $auth) {
+      var dfd = $q.defer();
+      if ($auth.isAuthenticated()) {
+        dfd.reject();
+      } else {
+        dfd.resolve();
+      }
+      return dfd.promise;
+    }
+    function loginRequired($q, $location, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/login');
+      }
+      return deferred.promise;
+    }
+});
