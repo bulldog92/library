ab9dbffc1aabd36a647e2d1188a3b5f0cd9bccbb
skost <skost@syneforge.com>
2016-02-02 16:59:52 +0000

    forgot password

--- public/js/controllers/forgotpass.js
+++ public/js/controllers/forgotpass.js
@@ -1,10 +1,13 @@
-app.controller('forgotpassCtrl',['$scope', 'Account', function($scope, Account){
+app.controller('forgotpassCtrl',['$scope', 'Account', '$location', '$timeout', function($scope, Account, $location, $timeout){
 	$scope.forgotAccount = {};
 	$scope.forgotSubmit = function(){
 		var promise = Account.forgotPass($scope.forgotAccount);
 		promise.then(function(data){
 			if(data.status == '200'){
 				$scope.successMessage = 'Новый пароль отправлен на почту';
+				$timeout(function(){
+					$location.path('/login');
+				}, 1500);
 			}else{
 				$scope.errorMessage = 'При отправке произошла ошибка';
 			}
