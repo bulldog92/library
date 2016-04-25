67f191ae43be135db15a18e9e5c270265519437c
Pasquill <pasquill.x@gmail.com>
2016-04-25 03:15:07 -0700

    cross rate count & tax payers

--- public/js/controllers/calculator.js
+++ public/js/controllers/calculator.js
@@ -1,3 +1,49 @@
 app.controller('calculatorCtrl',['$scope', function($scope){
-	$scope.test = 'It is work';
+	
+	/* Defaults */
+	$scope.showResults = false;
+	$scope.showResultsEUR = false;
+	$scope.showResultsTP = false;
+
+	$scope.data = {
+		salary: 1000,
+		plan: 100,
+		cross: .9,
+		rate: 28.5,
+		transfer: 5000,
+	};
+
+	/* Basic values to count */
+	$scope.bonus = function() {
+		return (Number($scope.data.salary) * ( Number($scope.data.plan) * 0.003 ));
+	}
+	$scope.summ = function() {
+		return $scope.data.salary + $scope.bonus();
+	}
+	$scope.totalTP = function() {
+		return (($scope.data.salary + $scope.bonus()) * $scope.data.cross) - ($scope.data.transfer/$scope.data.rate);
+	}
+	/* Switcher for tax payers' block */
+	$scope.message = "€";
+	$scope.changeCurrency = function(cbState) {
+  		$scope.message = cbState ? "$" : "€";
+  	};
+
+
+  	/* Primary count */
+	$scope.count = function() {
+		$scope.showResults = true;
+	};
+	$scope.showEUR = function() {
+		$scope.showResultsEUR = !$scope.showResultsEUR;
+	};
+
+
+	/* Count for tax payers */
+	$scope.showTP = function() {
+		$scope.showBlockTP = !$scope.showBlockTP;
+	};
+	$scope.countTP = function() {
+		$scope.showResultsTP = true;
+	};
 }]);
